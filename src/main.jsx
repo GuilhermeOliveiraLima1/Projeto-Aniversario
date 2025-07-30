39939bc626c7f686c5daeb09da774c9b20b177b5
GuilhermeOliveira01 <guilhermeoliveira0613@gmail.com>
2025-07-30 10:01:26 -0300

    delete rotas retiradas

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import Home from './pages/home/Home.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Timeline from './pages/timeline/Timeline.jsx'
 import Songs from './pages/songs/Songs.jsx'
-import LoveYouBecause from './pages/love you because/LoveYouBecause.jsx'
-import MiniGame from './pages/miniGames/MiniGame.jsx'
 
 createRoot(document.getElementById('root')).render(
 
