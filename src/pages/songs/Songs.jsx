bb4d1b855fdce54d0bf2bf8b3297c2708815e48e
GuilhermeOliveira01 <guilhermeoliveira0613@gmail.com>
2025-07-30 13:24:41 -0300

    Songs

--- src/pages/songs/Songs.jsx
+++ src/pages/songs/Songs.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import BottomMenu from '../../components/menu/Menu'
 import Header from '../../components/header/Header'
 
-export default function songs() {
+export default function Songs() {
   return (
     <div>
       <Header />
