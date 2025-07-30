9121912fdb899a74f79175bd8c29e6f003b1f66d
GuilhermeOliveira01 <guilhermeoliveira0613@gmail.com>
2025-07-30 10:20:21 -0300

    debug

--- src/pages/timeline/Timeline.jsx
+++ src/pages/timeline/Timeline.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
 import BottomMenu from '../../components/menu/Menu'
 import Header from '../../components/header/Header'
 import './Timeline.css'
-import TimelineCarousel from '../../components/carrosel/Carrosel';
 import Carrosel from '../../components/carrosel/Carrosel';
-
-
 
 export default function Timeline() {
   return (
